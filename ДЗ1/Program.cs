bf212d800bd1c2231814de4ffce0d7f64126eaae
PolinaVdovkina04 <Pouline.vdovkina@gmail.com>
2022-10-09 16:11:28 +0500

    Добавьте файлы проекта.

---
+++ ДЗ1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Владимир Маяковский");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Послушайте!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nПослушайте!\nВедь, если звёзды зажигают -\nзначит - это кому-нибудь нужно?\nЗначит - кто-то хочет, чтобы они были?\nЗначит - кто-то называет эти плевочки жемчужиной?");
+            Console.ResetColor();
+        }
+    }
+}
