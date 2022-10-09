f4c75dd38952146fe77342d96fb697944664f018
PolinaVdovkina04 <Pouline.vdovkina@gmail.com>
2022-10-09 19:36:29 +0500

    Задание 3 выполнено

---
+++ Задание 3/Задание 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трёхзначное число");
+            int a = int.Parse(Console.ReadLine());
+
+            int x = (a % 100) * 10 + (a / 100);
+
+            Console.WriteLine("Полученное число: " + x);
+        }
+    }
+}
