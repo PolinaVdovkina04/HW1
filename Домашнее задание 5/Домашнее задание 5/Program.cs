e71449e47e10398efc1f97625fa1a151c13c5150
PolinaVdovkina04 <Pouline.vdovkina@gmail.com>
2022-12-19 22:17:11 +0500

    Задание 5 выполнено

---
+++ Домашнее задание 5/Домашнее задание 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашнее_задание_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a = Program.Formula(2, 5);
+            double b = Program.Formula(3, 7);
+            double c = Program.Formula(5,11);
+
+            double x = a + b + c;
+
+            Console.WriteLine(Math.Round(x, 2));
+
+        }
+        static double Formula(double y, double z)
+        {
+            return (y + Math.Sin(y)) / (z + Math.Cos(z));
+        }
+    }
+}
