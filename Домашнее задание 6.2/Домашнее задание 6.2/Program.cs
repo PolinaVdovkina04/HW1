3009c8244099bd66a5e69fd1dfb035ea245ce9e5
PolinaVdovkina04 <Pouline.vdovkina@gmail.com>
2022-12-27 10:00:51 +0500

    Задание 6 выполнено частично

---
+++ Домашнее задание 6.2/Домашнее задание 6.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашнее_задание_6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string WordToChange = "апельсин";
+
+            string ChangedWord1 = WordToChange[5] + WordToChange[1] + WordToChange[0] + WordToChange[7] + WordToChange[6] + WordToChange[2] + WordToChange.Trim(new char[] { 'а', 'п', 'е', 'с', 'и', 'н' });
+            Console.WriteLine(ChangedWord1);
+
+            string ChangedWord2 = WordToChange.Remove(3, 5) + WordToChange[7] + WordToChange[4];
+            Console.WriteLine(ChangedWord2.Substring(1));
+        }
+    }
+}
